c7543b65491a40e428416b410cb1c44e619dc4f0
chrisbodon <bodon.cb@gmail.com>
2020-01-17 14:03:39 +0100

    boton changes

--- src/app/componentes/boton/boton.component.ts
+++ src/app/componentes/boton/boton.component.ts
@@ -9,7 +9,46 @@ export class BotonComponent implements OnInit {
 
   constructor() { }
 
+  numero = 0;
+
   ngOnInit() {
+
+
+  }
+
+  sumar: true;
+
+  operarNumeros = function () {
+    console.log("Hemos hecho click");
+    if (this.numero >= 0 && this.sumar == true) {
+
+      if (this.numero == 10) {
+
+        this.sumar = false;
+        this.numero--;
+
+      } else {
+
+        this.numero++;
+
+      }
+
+
+    } else {
+
+      if (this.numero == 0) {
+
+        this.sumar = true;
+        this.numero++;
+
+      } else {
+
+        this.numero--;
+
+      }
+
+
+    }
   }
 
 }
