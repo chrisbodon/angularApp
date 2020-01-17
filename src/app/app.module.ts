319f960060966c45877975b7387322ce3d149fb6
chrisbodon <bodon.cb@gmail.com>
2020-01-17 13:03:39 +0100

    New components and tests added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,14 @@ import { AppComponent } from './app.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BotonComponent } from './componentes/boton/boton.component';
+import { PruebaComponent } from './componentes/prueba/prueba.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BotonComponent,
+    PruebaComponent
   ],
   imports: [
     BrowserModule,
