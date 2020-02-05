92aff7bac89e2c4827625a94da7c595144e84bf2
chrisbodon <bodon.cb@gmail.com>
2020-02-05 13:59:12 +0100

    Filters and pipes examples

--- src/app/paginas/filtros/filtros.component.ts
+++ src/app/paginas/filtros/filtros.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ANIMALES } from 'src/app/animales';
 
 @Component({
   selector: 'app-filtros',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FiltrosComponent implements OnInit {
 
-  constructor() { }
+  coche: any;
+  animales: Array<any>;
+
+  constructor() {
+    console.trace('constructor');
+    this.coche = {
+      "nombre": "Audi R8",
+      "color": "blanco",
+      "isDiesel": false,
+      "precio": 10000
+    };
+
+    this.animales = ANIMALES;
+
+  } // constructor
 
   ngOnInit() {
-  }
+    console.trace('ngOnInit');
+
+  } // ngOnInit
 
 }
